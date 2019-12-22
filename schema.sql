3472069bdfed07e8bac5ec9ffd42d1a7e97ded8f
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2019-12-22 16:00:56 -0500

    yeah

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE managment_db;
+
+USE managment_db;
+
+CREATE TABLE employee(
+first_name varchar(30),
+last_name varchar(30),
+);
+
+CREATE TABLE department(
+department_name varchar(30),
+department_id INTEGER(4) auto_increment not null,
+primary key (department_id) 
+);
+
+CREATE TABLE role(
+title varchar(30),
+salary decimal
+);
+
